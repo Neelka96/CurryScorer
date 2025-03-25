49f579c086176ff86c58cdcde43b95baf855e7fe
Neel Agarwal <neelkagarwal@gmail.com>
2025-03-25 16:40:14 -0500

    2nd test hello world azure deployment

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
-print('Hello World!')
+from flask import Flask
+app = Flask(__name__)
 
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
 
 if __name__ == '__main__':
-    print('This is the if line, testing...')
+    pass
