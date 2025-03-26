02f08b25f0f441736fe5a065151cf6ed114072a7
Neel Agarwal <neelkagarwal@gmail.com>
2025-03-26 00:19:18 -0500

    Add sys module import and print sys.path for debugging

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import config as C
 
 # Import dependencies
 from pathlib import Path
+import sys
 
 
 
@@ -12,6 +13,7 @@ from pathlib import Path
 # if that doesn't work than move to blueprinting Flask or creating it modularly down stream
 with app.app_context():
     # Run All DB Tests and Ops
+    print(sys.path)
     main.run_db_ops(C.DB_PATH, C.NYC_OPEN_KEY)
 
 # Serve up flask API
